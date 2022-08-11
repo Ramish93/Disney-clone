eda72554ddd713567cde0067a78fda3eead5c092
Ramish Hassan <ramishshah93@gmail.com>
2022-08-12 01:08:20 +0200

    create new component for movie thumbnail, change next.config for the tmdb fetch, styled Ui of thumbnail component

--- components/MoviesCollection.js
+++ components/MoviesCollection.js
@@ -1,5 +1,14 @@
-const MoviesCollection = () => {
-  return <div>MoviesCollection</div>;
+import MovieThumbnail from "./MovieThumbnail";
+
+const MoviesCollection = ({ results, title }) => {
+  return (
+    <div className="relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] max-auto">
+      <h2 className="font-semibold">{title}</h2>
+      {results.map((result) => (
+        <MovieThumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 };
 
 export { MoviesCollection };
